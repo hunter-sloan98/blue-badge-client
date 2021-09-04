a59b9ffff06ccbfe1fc35d7029e09ad386a0a7c8
bscott125 <bwinfieldscott125@gmail.com>
2021-09-04 10:20:39 -0400

    Navbar

---
+++ blue-badge-client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Sitebar from './components/home/Navbar'
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Sitebar />
+    </div>
+  );
+}
+
+export default App;
